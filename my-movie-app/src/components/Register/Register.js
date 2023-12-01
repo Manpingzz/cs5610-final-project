49d1ad956bf94f2e58bc0eda6ef6ffb58f4cd6ea
Manpingzz <zhao.manp@northeastern.edu>
2023-12-01 14:51:08 -0800

    Update Register.js

--- my-movie-app/src/components/Register/Register.js
+++ my-movie-app/src/components/Register/Register.js
@@ -1,59 +1,89 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./index.css";
+import * as client from "../client";
 
 function Register() {
-  const [formData, setFormData] = useState({
+  const [credentials, setCredentials] = useState({
     username: "",
-    email: "",
     password: "",
-    confirmPassword: "",
+    email: "",
+    firstName: "",
+    lastName: "",
   });
+  const [error, setError] = useState("");
 
   const handleInputChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Perform validation and submit data
+
+    try {
+      await client.signup({
+        username: credentials.username,
+        email: credentials.email,
+        password: credentials.password,
+        firstName: credentials.firstName,
+        lastName: credentials.lastName,
+      });
+      // 处理注册成功的情况，例如重定向到登录页面
+    } catch (err) {
+      // 处理注册失败的情况
+      setError(err.message || "Signup failed.");
+    }
   };
 
   return (
     <div className="register-container">
       <h2>Create Your Account</h2>
+      {error && <div className="error-message">{error}</div>}
       <form onSubmit={handleSubmit} className="register-form">
         <input
           type="text"
           name="username"
-          value={formData.username}
+          value={credentials.username}
           onChange={handleInputChange}
           placeholder="Username"
           required
         />
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleInputChange}
-          placeholder="Email"
-          required
-        />
+
         <input
           type="password"
           name="password"
-          value={formData.password}
+          value={credentials.password}
           onChange={handleInputChange}
           placeholder="Password"
           required
         />
         <input
-          type="password"
-          name="confirmPassword"
-          value={formData.confirmPassword}
+          type="email"
+          name="email"
+          value={credentials.email}
           onChange={handleInputChange}
-          placeholder="Confirm Password"
+          placeholder="Email"
           required
         />
+
+        <input
+          type="text"
+          name="firstName"
+          value={credentials.firstName}
+          onChange={handleInputChange}
+          placeholder="First Name"
+          required
+        />
+
+        <input
+          type="text"
+          name="lastname"
+          value={credentials.lastName}
+          onChange={handleInputChange}
+          placeholder="Last Name"
+          required
+        />
+
         <button type="submit" className="register-btn">
           Sign Up
         </button>
